fe1a3fd89d5a6f41e20f0f34954e2a59abf4beca
Ksennia <liriena95@gmail.com>
2024-02-04 16:21:34 +0300

    finallyadd

--- phonebook.py
+++ phonebook.py
@@ -11,9 +11,7 @@
 import os 
 
 def add_new_user(name: str, phone: str, filename: str):
-    """
-    Добавление нового пользователя.
-    """
+    
     with open(filename, 'r+t', encoding='utf-8') as wrtbl:
        #l = wrtbl.readlines()
        lins_count = len(wrtbl.readlines())
@@ -21,19 +19,15 @@ def add_new_user(name: str, phone: str, filename: str):
 
 
 def read_all(filename: str) -> str:
-    """
-    Возвращает все содержимое телефонной книги.
-    """
+    
     with open(filename, 'r', encoding= 'utf-8') as data:
         resolt = data.read()
     return resolt    
 
 
-def search_user(data: str) -> str:
-    """
-    Поиск записи по критерию data.
-    """
-    with open(filename, 'r', encoding = 'UTF-8') as content:
+def search_user(data: str, filename: str) -> str:
+
+    with open(filename,'r', encoding = 'UTF-8') as content:
         text = content.readlines()
         res = ([item for item in text if data.lower() in item.lower()])
     return (''.join(res)).replace(';', ' ') if res else 'Вхождений не найдено'
@@ -43,12 +37,31 @@ def check_directory(filename: str):
         with open(filename, 'w', encoding = 'utf-8') as data:
             data.write("")
 
+def change_phone_number(filename: str):
+    with open(filename, 'w', encoding='utf-8') as file:
+        for contact in filename:
+            line = ' '.join(contact) + '\n'
+            file.write(line)
+
+
+def delete_contact(filename: str):
+    print("Введите контакт: ")
+    name = input('> ')
+    for contact in filename:
+        if contact['name'] == name:
+            print("Удалить контакт %s (YES/NO)?: " % name)
+            name_del = input('> ')
+            if name_del == "YES":
+                filename.remove(contact)
+                print("Вы удалили контакт %s " % name)
 
 INFO_STRING = """
 Выберите ркжим работы:
 1 - вывести все данные
 2 - добавление нового пользователя
 3 - поиск
+4 - изменение контакта
+5 - удаление контакта
 """
 DATASOURCE = 'phone.txt'
 
@@ -63,6 +76,20 @@ while True:
         phone = input()
         add_new_user(name=user, phone=phone, filename=DATASOURCE)
     elif mode == 3:
-        earch = input('Введите строку для поиска: ')
-        print(search_user(search, DATA_SOURCE))
+        search = input('Введите строку для поиска: ')
+        print(search_user(search, DATASOURCE))
         exit()
+    elif mode == 4:
+            number_to_change = search_user(DATASOURCE)
+            print('Какое поле вы хотите изменить?')
+            field = input('1 - Фамилия\n2 - Имя\n3 - Номер телефона\n')
+            if field == '1':
+                number_to_change[0] = input('Введите фамилию: ')
+            elif field == '2':
+                number_to_change[1] = input('Введите имя: ')
+            elif field == '3':
+                number_to_change[2] = input('Введите номер телефона: ')
+            DATASOURCE.append(number_to_change)
+    elif mode == 5:
+            search = input("Введите контакт: ")
+            print(delete_contact(DATASOURCE)) 
